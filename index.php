1e8c1206e07304ee358ffb9b6a14ca6826a68d5f
sivakumartskl <sivakumartskl5a2@gmail.com>
2017-08-04 15:45:29 +0530

    Updated login and registration forms

--- index.php
+++ index.php
@@ -27,27 +27,55 @@ session_start();
     }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Welcome to login page</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/jquery-2.1.4.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </head>
     <body>
-        <div>
-            <form action='#' method='post'>
-                <div>
-                    <label>Username : </label>
-                    <div><input type='text' name='username'></div>
+        <div class="container-fluid">
+            <div class="jumbotron text-center">
+                <h1>Vit Cse B</h1>
+                <p>2011 - 2015</p> 
+                <p>Facebook type website for vit friends</p> 
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-container">
+                        <form action='#' method='post' class="form-horizontal">
+                            <div class="form-group">
+                                <label class="control-label col-sm-offset-3 col-sm-2">Username</label>
+                                <div class="col-sm-3"><input type='text' name='username' class="form-control"></div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-sm-offset-3 col-sm-2">Password</label>
+                                <div class="col-sm-3"><input type='password' name='password' class="form-control"></div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-5 col-sm-3">
+                                    <input type='submit' name='logsub' class="btn btn-default login-button" value="Login">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-sm-offset-3 col-sm-2">Don't have an account?</label>
+                                <div class="col-sm-3">
+                                    <a href="registration.php" class="btn btn-success form-redirect-btn">Register Here</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div>
-                    <label>Password : </label>
-                    <div><input type='password' name='password'></div>
-                </div>
-                <div>
-                    <div><input type='submit' name='logsub'></div>
-                </div>
-            </form>
+            </div>
+            <footer class="footer">
+              <div class="container">
+                <p class="text-muted">A webiste by Sivakumar Tadisetti...</p>
+              </div>
+            </footer>
         </div>
-        <a href="registration.php">Register here...</a>
     </body>
 </html>
--- registration.php
+++ registration.php
@@ -24,23 +24,73 @@ if (isset($_SESSION['vitcseb1115users']) && isset($_SESSION['vitcseb1115userid']
     }
 }
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Registration page</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/jquery-2.1.4.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </head>
     <body>
-        <form action="#" method="POST">
-            <label>Username</label>
-            <input type="text" name="username" placeholder="Username">
-            <label>Email</label>
-            <input type="email" name="email" placeholder="Email address">
-            <label>password</label>
-            <input type="password" name="password" placeholder="Password">
-            <label>college registration number</label>
-            <input type="text" name="colregnum" placeholder="college registration number">
-            <input type="submit" value="Register" name="regsubmit">
-        </form>
-        <a href="index.php">Login here...</a>
+        <div class="container-fluid">
+            <div class="jumbotron text-center">
+                <h1>Vit Cse B</h1>
+                <p>2011 - 2015</p> 
+                <p>Facebook type website for vit friends</p> 
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-container">
+                        <form action="#" method="POST" class="form-horizontal">
+                            <div class="form-group">
+                                <label class="control-label col-sm-offset-3 col-sm-2">Username</label>
+                                <div class="col-sm-3">
+                                    <input type="text" name="username" placeholder="Username" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-sm-offset-3 col-sm-2">Email</label>
+                                <div class="col-sm-3">
+                                    <input type="email" name="email" placeholder="Email address" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-sm-offset-3 col-sm-2">password</label>
+                                <div class="col-sm-3">
+                                    <input type="password" name="password" placeholder="Password" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-sm-offset-3 col-sm-2">college registration number</label>
+                                <div class="col-sm-3">
+                                    <input type="text" name="colregnum" placeholder="college registration number" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-5 col-sm-3">
+                                    <input type="submit" value="Register" name="regsubmit" class="btn btn-default login-button">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-sm-offset-3 col-sm-2">Already have an account?</label>
+                                <div class="col-sm-3">
+                                    <a href="index.php" class="btn btn-success form-redirect-btn">Login Here</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <footer class="footer">
+              <div class="container">
+                <p class="text-muted">A webiste by Sivakumar Tadisetti...</p>
+              </div>
+            </footer>
+        </div>
     </body>
 </html>
 
